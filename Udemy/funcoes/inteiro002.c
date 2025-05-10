0eb126691285d5718531b6160291ac17277b1181
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-05-09 21:46:53 -0300

    Atualização

---
+++ Udemy/funcoes/inteiro002.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int somar(int a , int b){
+return a+b;
+}
+
+int main(){
+
+int a , b;
+
+a= 10;
+b=20;
+
+printf("%d\n" ,somar(a,b));
+
+
+    return 0;
+}
