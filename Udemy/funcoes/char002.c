a5d96e36c3f348e28fa2548149068f6879c72b16
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-05-09 21:51:05 -0300

    Atualização

---
+++ Udemy/funcoes/char002.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char primeiraLetra(){
+    char letra = 'a';
+    return letra;
+}
+
+int main(){
+    
+    printf("%c" , primeiraLetra());
+    return 0;
+}
