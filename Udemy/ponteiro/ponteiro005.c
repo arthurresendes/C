c801a678f5fc03d47394750b4c0043173a4f6e91
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-05-21 17:27:50 -0300

    Atualização

--- Udemy/ponteiro/ponteiro005.c
+++ Udemy/ponteiro/ponteiro005.c
@@ -3,6 +3,19 @@
 
 int main(){
 
-    
+    int n1 = 20;
+
+    printf("%d \n" , n1);
+
+    int* incrementa = &n1;
+    n1 = 30;
+    ++(*incrementa);
+
+    printf("%d \n" , n1);
+
+    printf("%d \n" , &n1);
+    printf("%d \n" , &incrementa);
+
+
     return 0;
 }
