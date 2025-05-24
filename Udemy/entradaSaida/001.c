d38a96e3b970fbf23304a6e564f5c4ae93d16b97
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-05-23 23:11:29 -0300

    Atualização

--- Udemy/entradaSaida/001.c
+++ Udemy/entradaSaida/001.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hello world!!");
+    //FILE = Arquivo
+    FILE *arq;
+
+    //Fopen = Abre um arquivo
+    //w- Abre o arquivo pra escrita
+    //a- Abre o arquivo pra escrita, se não existir ele cria
+    //r- Abre o arquivo pra leitura
+    //wa- abri o arquivo para a adição de conteudo
+    arq = fopen("arquivo.txt" , "a");
+
+    //Sempre devemos fechar-los
+    fclose(arq);
     return 0;
 }
 
