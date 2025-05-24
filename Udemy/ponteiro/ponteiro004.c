a0e8e2418ee6b5d6867b64dac50a8f450806b26a
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-05-23 22:20:05 -0300

    Atualização

--- Udemy/ponteiro/ponteiro004.c
+++ Udemy/ponteiro/ponteiro004.c
@@ -20,20 +20,28 @@ int main()
 
     printf("\n N1:%d , N2:%d", n1, n2);
 
-    if (&n1 > &n2)
+    int* p1;
+    int* p2;
+    p1 = &n1;
+    p2 = &n2;
+    int* resul;
+    resul = p1-p2;
+
+    if (p1 > p2)
     {
 
-        printf("\n Ponteiro de N1 eh maior %p", (void *)&n1);
+        printf("\n Ponteiro de N1 eh maior %p", p1);
 
-        printf("\n Conteúdo no maior endereço: %d\n", n1);
+        printf("\n Conteúdo no maior endereço: %p\n", p1);
     }
     else
     {
 
-        printf("\n Ponteiro de N2 eh maior %p", (void *)&n2);
+        printf("\n Ponteiro de N2 eh maior %p", p2);
 
-        printf("\n Conteúdo no maior endereço: %d\n", n2);
+        printf("\n Conteúdo no maior endereço: %p\n", p2);
     }
+    printf("\n A diferença entre eles foram de %p - %p = %p" , p1 , p2 , resul);
 
     return 0;
 }
