82d587df5ad9ac232231ef1781f5e4f941ec6cda
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-05-28 17:26:34 -0300

    Atualização

--- Udemy/entradaSaida/002.c
+++ Udemy/entradaSaida/002.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
-int main(){
+int main() {
+    char c;
+    FILE *arq;
+
+    // Corrigido o nome do arquivo
+    arq = fopen("arquivo01.txt", "r");
+
+    if (arq) {
+        while ((c = getc(arq)) != EOF) {
+            printf("%c", c);
+        }
+        fclose(arq);
+        printf("\n");
+    } else {
+        printf("Não achei o arquivo\n");
+    }
 
     return 0;
 }
+
