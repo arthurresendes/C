19d4f14a641abea7ea1c2b02394980b22779254a
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-06-04 17:40:02 -0300

    Atualização

--- Udemy/structs/struct006.c
+++ Udemy/structs/struct006.c
@@ -1,23 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 struct pessoas
 {
     char nome[100];
-    int idade;
+    char idade[20];
 };
+
+int proibeNum(char *str)
+{
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        if (isdigit(str[i])  && str[i] != ' ' && str[i] != '\n')
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int proibeLetra(char *str)
+{
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        if (!isdigit(str[i]) && str[i] != '\n')
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
 
 int main()
 {
+    
     struct pessoas p;
+    int idadeNum;
     system("cls");
-    printf("Digite seu nome: ");
-    fgets(p.nome, sizeof(p.nome), stdin);
+    do
+    {
+        printf("Digite seu nome: ");
+        fgets(p.nome, sizeof(p.nome), stdin);
+    } while (!proibeNum(p.nome) || strlen(p.nome) == 0);
     p.nome[strcspn(p.nome, "\n")] = '\0';
+    do{
     printf("Digite sua idade: ");
-    scanf("%d", &p.idade);
+    fgets(p.idade, sizeof(p.idade), stdin);
+    }while(!proibeLetra(p.idade) || strlen(p.idade) == 0);
+    idadeNum = atoi(p.idade);
     system("cls");
-    printf("Seu nome é %s e voce tem %d anos ", p.nome, p.idade);
+    printf("Seu nome é %s e voce tem %s anos ", p.nome, p.idade);
     return 0;
 }
