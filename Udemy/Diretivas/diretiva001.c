f32d91cdbfc2cac9024d3e15dcbb235979dbbae9
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-06-11 19:59:53 -0300

    Atualização

--- Udemy/Diretivas/diretiva001.c
+++ Udemy/Diretivas/diretiva001.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+//indef - Diretivas de compilação
+//Nao conseguimos alterar valor de constante, ao menso que usamos #ifndef
+
+#ifndef PI
+    #define PI 3.14567
+#endif
+
 int main(){
-    printf("Hello world!!");
+    printf("%f" , PI);
+    printf("\n Alterando constante %f" , 2 * PI);
     return 0;
 }
 
