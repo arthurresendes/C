507fa9fc7e013071920db085584ce18a75b4ae1c
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-06-13 19:53:47 -0300

    Atualização

--- EstudosCurso/Bibliotecas/ExerciciosStdilib/absAndDiv.c
+++ EstudosCurso/Bibliotecas/ExerciciosStdilib/absAndDiv.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
     int n1 = -10;
     int absoluto =  abs(n1);
-    printf("Valor de n1 eh: %d" , n1);
-    printf("Valor absoluto de n1 eh: %d" , absoluto);
+    int n2 = 23;
+    int n3 = 2;
+
+    div_t quiocente = div(n2 , n3);
+    printf("\nValor de n1 eh: %d" , n1);
+    printf("\nValor absoluto de n1 eh: %d" , absoluto);
+    printf("\nQuiocente de n2(%d) e de n3(%d) eh: %d" ,n2,n3, quiocente.rem);
     return 0;
 }
 
