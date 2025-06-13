60dcdff3ae69663c4c7d366d0d5db2cb2bd1d343
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-06-13 20:12:06 -0300

    Atualização

---
+++ EstudosCurso/Bibliotecas/mat.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+
+//cos(calcula o cosseno de um angulo em radianos), sin(calcula o seno de um angulo em radianos)
+
+int main(){
+    printf("Hello world!!");
+    return 0;
+}
+
