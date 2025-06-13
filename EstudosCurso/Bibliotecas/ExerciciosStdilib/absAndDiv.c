6c5ae615e153f19c4f19f15fb09c314df47e71a0
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-06-13 19:50:19 -0300

    Atualização

---
+++ EstudosCurso/Bibliotecas/ExerciciosStdilib/absAndDiv.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int n1 = -10;
+    int absoluto =  abs(n1);
+    printf("Valor de n1 eh: %d" , n1);
+    printf("Valor absoluto de n1 eh: %d" , absoluto);
+    return 0;
+}
+
