dce8162d5d97ee3011ddda35d8e931312ac916f1
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-06-13 20:24:07 -0300

    Atualização

---
+++ EstudosCurso/Bibliotecas/ExerciciosMath/logBase10.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    double a = 42;
+    double b = 20;
+    double c = 33;
+
+    printf("O exponencial de %lf eh %lf\n" , a , log10(a));
+    printf("O exponencial de %lf eh %lf\n" , b , log10(b));
+    printf("O exponencial de %lf eh %lf\n" , c , log10(c));
+    return 0;
+}
