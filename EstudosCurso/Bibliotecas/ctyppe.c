79dd1a7ae6860c6ebdaaa3f253fda73b9b0fe4e4
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-06-13 20:38:15 -0300

    Atualização

--- EstudosCurso/Bibliotecas/ctyppe.c
+++ EstudosCurso/Bibliotecas/ctyppe.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <ctype.h>
 
+//Teste de tipos de caracteres
 
+//isalnum(verifica se o caractere é alfanumerico),isalpha(verifica se o caractere é alfabetico) , isdigit(verifica se é um digito decimal) , ispunct(verifica se o caractere é uma pontuação) , ispace(verifica se é um espaco), isupper(verifica se o caractere é maiusculo) ,islower(verifica se é minuscula),tolower(converte para minuscula), toupper(converte para maiuscula)
 
 int main(){
     printf("Hello world!!");
