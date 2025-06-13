3758aa3271276f6ca101e7b78dac818ae304a4e5
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-06-13 20:16:27 -0300

    Atualização

--- EstudosCurso/Bibliotecas/ExerciciosMath/hiperbolico.c
+++ EstudosCurso/Bibliotecas/ExerciciosMath/hiperbolico.c
@@ -3,9 +3,9 @@
 
 int main(){
     int angulo = 45;
-    float res = cos(angulo);
-    float seno = sin(angulo);
-    float tg = tan(angulo);
+    float res = cosh(angulo);
+    float seno = sinh(angulo);
+    float tg = tanh(angulo);
     printf("O cosseno de %d eh %f" , angulo , res);
     printf("\nO seno de %d eh %f" , angulo , seno);
     printf("\nA tangente de %d eh %f" , angulo , tg);
--- EstudosCurso/Bibliotecas/mat.c
+++ EstudosCurso/Bibliotecas/mat.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 
-//cos(calcula o cosseno de um angulo em radianos), sin(calcula o seno de um angulo em radianos) , tang(calcula a tangente de um angulo em radiano)
+//cos(calcula o cosseno de um angulo em radianos), sin(calcula o seno de um angulo em radianos) , tan(calcula a tangente de um angulo em radiano), cosh(calcula o cosseno hiperbolico de um angulo em radianos),sinh(calcula o seno hiperbolico de um angulo em radianos),tanh(calcula a tangente hiperbolico de um angulo em radianos)
 
 int main(){
     printf("Hello world!!");
