cffbc0b2c1776cb84696a30946e648bf0eb18411
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-06-13 20:13:25 -0300

    Atualização

--- EstudosCurso/Bibliotecas/ExerciciosMath/angulos.c
+++ EstudosCurso/Bibliotecas/ExerciciosMath/angulos.c
@@ -4,7 +4,11 @@
 int main(){
     int angulo = 45;
     float res = cos(angulo);
+    float seno = sin(angulo);
+    float tg = tang(angulo);
     printf("O cosseno de %d eh %f" , angulo , res);
+    printf("O seno de %d eh %f" , angulo , seno);
+    printf("A tangente de %d eh %f" , angulo , tg);
     return 0;
 }
 
--- EstudosCurso/Bibliotecas/mat.c
+++ EstudosCurso/Bibliotecas/mat.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 
-//cos(calcula o cosseno de um angulo em radianos), sin(calcula o seno de um angulo em radianos)
+//cos(calcula o cosseno de um angulo em radianos), sin(calcula o seno de um angulo em radianos) , tang(calcula a tangente de um angulo em radiano)
 
 int main(){
     printf("Hello world!!");
