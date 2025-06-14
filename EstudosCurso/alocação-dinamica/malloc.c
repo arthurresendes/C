7c1a71e9cc1347cdb77497c6a92ee3262cfbe9ab
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-06-14 20:20:27 -0300

    Atualização

--- EstudosCurso/alocação-dinamica/malloc.c
+++ EstudosCurso/alocação-dinamica/malloc.c
@@ -5,7 +5,7 @@ int main(){
     int qtd, *p;
     printf("Digite a quantidade de elementos: ");
     scanf("%d" , &qtd);
-    p = (int*)malloc(qtd * sizeof(int));
+    p = (int*)malloc(qtd * sizeof(int)); 
 
     for(int i = 0 ; i <qtd ; i++){
         printf("Digite o valor do numero %d: " , i+1);
@@ -14,6 +14,11 @@ int main(){
     for(int i = 0 ; i <qtd ; i++){
         printf("Numero %d vale %d e ocupa %ld bytes em memoria \n",i+1 , p[i] , sizeof(p[i]));
     }
+
+    printf("Total de memoria alocada: %ld bytes\n", qtd * sizeof(int));
+
+    free(p); //libera a memoria(desalocar espaço)
+    p = NULL;
     return 0;
 }
 
