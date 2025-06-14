28a800203317625eaa7adc22a77f9d181f945624
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-06-13 21:30:58 -0300

    Atualização

--- EstudosCurso/Bibliotecas/ExerciciosTime/tempo.c
+++ EstudosCurso/Bibliotecas/ExerciciosTime/tempo.c
@@ -2,7 +2,12 @@
 #include <time.h>
 
 int main() {
-    
+    time_t segundos;
+
+    segundos = time(NULL);
+    printf("Horas des de janeiro de 1970: %ld\n" , segundos/3600);
+    printf("Minutos des de janeiro de 1970: %ld\n" , segundos/60);
+    printf("Segundos des de janeiro de 1970: %ld\n" , segundos);
 
     return 0;
 }
--- EstudosCurso/Bibliotecas/time.c
+++ EstudosCurso/Bibliotecas/time.c
@@ -3,6 +3,7 @@
 
 //clock(retorna o numero de pulsos de clocks desde que o programa iniciou)
 //time(retorna o tempo real)
+//localtime(converte valor time para uma hora local)
 
 int main(){
     printf("Hello world!!");
