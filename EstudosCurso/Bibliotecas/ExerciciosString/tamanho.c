aa62ff47a16385a859abd07a34fee0974b1d0abf
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-06-13 21:18:06 -0300

    Atualização

---
+++ EstudosCurso/Bibliotecas/ExerciciosString/tamanho.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str1[] = "pedro@gmail.com";
+
+    printf("Tamanho da string %d" , strlen(str1 ));
+
+
+    return 0;
+}
--- EstudosCurso/Bibliotecas/strings.c
+++ EstudosCurso/Bibliotecas/strings.c
@@ -6,6 +6,7 @@
 //Busca
 //strchr(realiza a primeira ocorrencia de caractere em uma string)
 //strtok(dividi uma string em substring)
+//strlen(retorna o tamanho da string)
 
 int main(){
     printf("Hello world!!");
