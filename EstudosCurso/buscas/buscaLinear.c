d3f03cdace6186388b2ed3b1ea24523662a97586
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-06-25 19:16:50 -0300

    Atualização

--- EstudosCurso/buscas/buscaLinear.c
+++ EstudosCurso/buscas/buscaLinear.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 
+int  buscaLinear(int vetor[] , int chave , int tam){
+    for(int i =0 ;i < tam ; i++){
+        if(vetor[i] == chave){
+            return i;
+        }
+    }
+    return -1;
+}
+
 int main(){
-    printf("Hello world!!");
+    int vetor[6] = {1,3,5,8,12,42};
+
+    //Elemento que quermos encontrar
+    int chave = 5;
+
+    int ret = buscaLinear(vetor , chave , 6);
+
+
+    printf("O elemento %d esta na posicao %d" , chave , ret);
     return 0;
 }
 
