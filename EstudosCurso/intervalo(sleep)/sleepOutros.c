9ef3cb4649cbdb79ed525a910bdc251e8aebc94c
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-06-26 19:07:59 -0300

    Atualização

---
+++ EstudosCurso/intervalo(sleep)/sleepOutros.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+
+#include <unistd.h>
+
+int main()
+{
+
+    printf("=== Seção 1: Introdução ===\n");
+
+    printf("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n\n");
+
+    sleep(2);
+
+    printf("=== Seção 2: Desenvolvimento ===\n");
+
+    printf("Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\n\n");
+
+    sleep(2);
+
+    printf("=== Seção 3: Conclusão ===\n");
+
+    printf("Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\n");
+
+    sleep(2);
+
+    return 0;
+}
