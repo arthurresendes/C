7b0a00309b14b5dff32a890b710186229144a95d
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-06-27 14:15:56 -0300

    Atualização

---
+++ EstudosCurso/funcoes/medium/medium001.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+float media(float n1, float n2, float n3)
+{
+
+    float media = (n1 + n2 + n3) / 3;
+
+    printf("A media eh de %.2f", media);
+}
+
+int main()
+
+{
+
+    float x, y, z;
+
+    printf("Digite nota 1 : ");
+
+    scanf("%f", &x);
+
+    printf("Digite nota 2 : ");
+
+    scanf("%f", &y);
+
+    printf("Digite nota 3 : ");
+
+    scanf("%f", &z);
+
+    media(x, y, z);
+
+    return 0;
+}
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+float media(float n1, float n2, float n3)
+{
+
+    float media = (n1 + n2 + n3) / 3;
+
+    printf("A media eh de %.2f", media);
+}
+
+int main()
+
+{
+
+    float x, y, z;
+
+    printf("Digite nota 1 : ");
+
+    scanf("%f", &x);
+
+    printf("Digite nota 2 : ");
+
+    scanf("%f", &y);
+
+    printf("Digite nota 3 : ");
+
+    scanf("%f", &z);
+
+    media(x, y, z);
+
+    return 0;
+}
